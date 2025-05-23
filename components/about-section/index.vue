cbd74129adc958b397f5c9d144b3f8b07c14bd9f
dzhivanyan <dzhivanyan>
2025-05-23 11:57:33 +0300

    update about

---
+++ components/about-section/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <section
+    id="about"
+    class="3xl:px-[134px] 3xl:py-[165px] 3xl:gap-[80px] flex flex-wrap justify-center gap-[20px] bg-white px-[20px] py-[50px] md:gap-[40px]"
+  >
+    <about-herb-gathering-card />
+    <about-arrival-dates-card />
+    <about-rooms-card />
+    <about-healthy-nutrition-card />
+    <about-gallery-card />
+    <about-wellness-tips-card />
+    <about-recommendations-card />
+    <about-our-doctors-card />
+    <about-creative-retreat-card />
+  </section>
+</template>
